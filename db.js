0919370d29f3e21bee54a98b73b68900217ab8e4
Kyle Ramey <kyle@codefusion.ca>
2017-05-07 19:45:27 -0300

    Using env file for db connection

--- db.js
+++ db.js
@@ -9,10 +9,10 @@ var db;
 
 function connect(){
     db = mysql.createConnection({
-        host: 'localhost',
-        user: 'hfxbusses',
-        password: 'alwayslate',
-        database: 'hfxbusses'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE
     });
     return db;
 }
